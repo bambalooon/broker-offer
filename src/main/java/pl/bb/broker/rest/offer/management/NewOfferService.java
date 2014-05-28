8024250168b4e26ff1ea1ae984b2abb8eadb6e39
bambalooon <k.bb@wp.pl>
2014-05-28 23:19:11 +0200

    OfferDetails processing added.

--- src/main/java/pl/bb/broker/rest/offer/management/NewOfferService.java
+++ src/main/java/pl/bb/broker/rest/offer/management/NewOfferService.java
@@ -1,10 +1,12 @@
 package pl.bb.broker.rest.offer.management;
 
 import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataBodyPart;
 import com.sun.jersey.multipart.FormDataParam;
 import org.apache.commons.io.IOUtils;
 import org.hibernate.HibernateException;
 import pl.bb.broker.brokerdb.broker.entities.CompaniesEntity;
+import pl.bb.broker.brokerdb.broker.entities.OfferDetailsEntity;
 import pl.bb.broker.brokerdb.broker.entities.OffersEntity;
 import pl.bb.broker.brokerdb.util.BrokerDBUtil;
 
@@ -15,6 +17,10 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -30,10 +36,12 @@ public class NewOfferService {
     @POST
     @Path("/site")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response createNewOffer(                             //only offer - details somewhere else..
+    public Response createNewOffer(                             //check file!
             @FormDataParam("image")final InputStream imageIS,
             @FormDataParam("image")final FormDataContentDisposition imageDetail,
             @FormDataParam("description") String description,
+            @FormDataParam("rooms") List<FormDataBodyPart> rooms,
+            @FormDataParam("prices") List<FormDataBodyPart> prices,
             @Context SecurityContext context //context to get username of logged company!
     ) {
         OffersEntity offer = new OffersEntity();
@@ -56,6 +64,23 @@ public class NewOfferService {
             return Response.status(500).entity("Error transering to byte[]").build();
         }
         offer.setImage(imgBytes);
+        List<OfferDetailsEntity> details = new ArrayList<>();
+        Iterator<FormDataBodyPart> pricesIt = prices.iterator();
+        for(FormDataBodyPart room : rooms) {
+            OfferDetailsEntity detail = new OfferDetailsEntity();
+            detail.setOffer(offer);
+            detail.setRoom(room.getValueAs(String.class));
+            if(pricesIt.hasNext()) {
+                FormDataBodyPart price = pricesIt.next();
+                try {
+                    detail.setPrice(BigDecimal.valueOf(Double.parseDouble(price.getValueAs(String.class))));
+                } catch (Exception e) {
+                    return Response.status(400).entity("Wrong price: "+price.getValueAs(String.class)).build();
+                }
+            }
+            details.add(detail);
+        }
+        offer.setDetails(details);
         try {
             BrokerDBUtil.INSTANCE.saveOffer(offer);
         } catch (HibernateException e) {
